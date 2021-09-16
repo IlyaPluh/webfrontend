40dea3dafda30d00e8666ecaf7f76e0ae1db7df8
IlyaPluh <ilyaandriyanov@mail.ru>
2021-09-16 19:13:57 +0300

    modal

--- study/src/js/Modal.js
+++ study/src/js/Modal.js
@@ -19,12 +19,13 @@ export class Modal {
 
         //Close button
         this.modalCloseBtn = this.createDomNode(this.modalCloseBtn, 'span', 'modal_close-icon');
-        this.modalCloseBtn.innerHTML = 
-        
+        this.modalCloseBtn.innerHTML = '<svg height="19pt" viewBox="0 0 329.26933 329" width="19pt" xmlns="http://www.w3.org/2000/svg"><path d="m194.800781 164.769531 128.210938-128.214843c8.34375-8.339844 8.34375-21.824219 0-30.164063-8.339844-8.339844-21.824219-8.339844-30.164063 0l-128.214844 128.214844-128.210937-128.214844c-8.34375-8.339844-21.824219-8.339844-30.164063 0-8.34375 8.339844-8.34375 21.824219 0 30.164063l128.210938 128.214843-128.210938 128.214844c-8.34375 8.339844-8.34375 21.824219 0 30.164063 4.15625 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921875-2.089844 15.082031-6.25l128.210937-128.214844 128.214844 128.214844c4.160156 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921874-2.089844 15.082031-6.25 8.34375-8.339844 8.34375-21.824219 0-30.164063zm0 0"/></svg>'
         this.setContent(content);
 
         this.appendModalElements();
 
+        //Bind events
+        this.bindEvents();
 
         //Open Modal
         this.openModal();
@@ -53,5 +54,18 @@ export class Modal {
 
     openModal() {
         document.body.append(this.overlay);
-    }
+    };
+
+    bindEvents() {
+        this.modalCloseBtn.addEventListener('click', this.closeModal);
+        this.overlay.addEventListener('click', this.closeModal);
+
+    };
+
+    closeModal(e) {
+        let classes = e.target.classList;
+        if(classes.contains('overlay') || classes.contains('modal_close-icon')) {
+            document.querySelector('.overlay').remove();
+        }
+    };
 }
